346368d7d98b011bc604fa78801068489be1d0e6
Nasser-dev <60063972+Nasser-dev@users.noreply.github.com>
2020-04-22 12:59:42 +0200

    Création version rattachement
    J'ai terminé la création des phrases en version rattachement avec son style ainsi que la gestion de crédit

---
+++ Enigmot/application/views/pages/profil.php
@@ -0,0 +1,13 @@
+<section class="banner_area">
+    <div class="banner_inner d-flex align-items-center" style="min-height: 100vh">
+        <div class="overlay bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background=""></div>
+        <div class="container">
+            <div class="banner_content text-center p-5">
+                <h2 style="text-transform: none;">Profil Nasser</h2>
+            </div>
+            <div class="col-lg-12 p-4" style="background-color: #5753967a;">
+
+            </div>
+        </div>
+    </div>
+</section>
