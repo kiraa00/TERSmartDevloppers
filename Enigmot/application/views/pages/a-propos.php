0740c17bb975d0ced782a347b81812137e00d8c6
Mohamed Hassan <demahom1998@gmail.com>
2020-05-01 16:27:52 +0200

    export page

--- Enigmot/application/views/pages/a-propos.php
+++ Enigmot/application/views/pages/a-propos.php
@@ -5,7 +5,20 @@
             <div class="banner_content text-center p-5" style="margin-top:50px;">
                 <h2 style="text-transform: none;">À propos</h2>
             </div>
-            
+            <div class="col-md-12">
+                <div class="col-lg-12 p-4" style="background-color: #5753967a;text-align:center;color:white">
+                    
+                       <p> Enigmot est un site web réalisé dans le cadre d'un TER (Travail d'Étude et de Recherche) de première année de master informatique .
+                            Il permet de produire des corpus de phrases annotées sémantiquement, permettant ainsi d'identifier le sens correct d'un mot.</p>
+                            <p><b>Tuteur :</b> Mathieu Lafourcade</p>
+                            <p><b>Membres :</b> Mohamed Hassan Ibrahim,Nasser Omar Soubagle,Bachir Rehhali , Chahinez Benallal</p>
+                            <p><b>Github :</b> <a   href="https://github.com/kiraa00/TERSmartDevloppers/tree/develop">Enigmots</a></p>
+                            <p><b>Version actuelle :</b>1.0.1 (20/05/2020)</p>
+                            <p> Enigmot est un site web réalisé dans le cadre d'un TER (Travail d'Étude et de Recherche) de première année de master informatique .
+                            Il permet de produire des corpus de phrases annotées sémantiquement, permettant ainsi d'identifier le sens correct d'un mot.</p>
+                
+                </div>    
+			</div>
 
             
         </div>
